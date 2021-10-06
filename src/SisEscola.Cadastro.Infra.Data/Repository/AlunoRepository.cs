0295bd49454ab5dacb6bd06d00f8ff5ce7b5d9ac
Thiago M S Arrais <thiagomsarrais@outlook.com>
2021-10-06 10:23:17 -0300

    Criação e implementação do respositório aluno

---
+++ src/SisEscola.Cadastro.Infra.Data/Repository/AlunoRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SisEscola.Cadastro.Domain.Models;
+using SisEscola.Cadastro.Domain.Models.Interfaces;
+using SisEscola.Cadastro.Infra.Data.Context;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SisEscola.Cadastro.Infra.Data.Repository
+{
+    public class AlunoRepository : IAlunoRepository
+    {
+        private readonly SisEscolaDbContext _sisEscolaDbContext;
+
+        public AlunoRepository(SisEscolaDbContext sisEscolaDbContext)
+        {
+            _sisEscolaDbContext = sisEscolaDbContext;
+        }
+
+        public async Task<Aluno> Adicionar(Aluno aluno)
+        {
+            await _sisEscolaDbContext.AddAsync(aluno);
+            await _sisEscolaDbContext.SaveChangesAsync();
+
+            return aluno;
+        }
+
+        public async Task<IEnumerable<Aluno>> ObterAlunos()
+        {
+            return await _sisEscolaDbContext.Alunos.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Aluno>> ObterAlunosComFiltro(string tipoFiltro, string consulta)
+        {
+            return await _sisEscolaDbContext.Alunos.Where(e => e.GetType().GetProperty(tipoFiltro).GetValue(tipoFiltro).ToString() == consulta).ToListAsync(); ;
+        }
+
+        public async Task<IEnumerable<Responsavel>> ObterResponsaveis()
+        {
+            return await _sisEscolaDbContext.Responsaveis.ToListAsync();
+        }
+    }
+}
